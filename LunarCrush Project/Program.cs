30a0c9dbcce9685bec833475661f2bdbf93ca68a
Léa Buendé <lea.buende.tchiendjou@efrei.net>
2021-10-15 19:32:51 +0200

    connecting to the api (the authorization doesnt work)

---
+++ LunarCrush Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Lunarcrush_Project
+{
+    public class DataObject
+    {
+        public string Name { get; set; }
+    }
+
+    public class Program
+    {
+        private const string URL = "https://api.lunarcrush.com/v2?data=assets?";
+        private static string urlParameters = "?api_key=y2p6tcgi3eoih3b9fbol0f";
+
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result; 
+            if (response.IsSuccessStatusCode)
+            {
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            client.Dispose();
+        }
+    }
+}
