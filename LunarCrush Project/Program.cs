4040661bd907cae947d30c459471d512ae544c88
Duc-Thomas <duckiduc.dev@gmail.com>
2021-10-16 16:59:41 +0200

    Fix api access

--- LunarCrush Project/Program.cs
+++ LunarCrush Project/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace Lunarcrush_Project
+namespace ConsoleProgram
 {
     public class DataObject
     {
@@ -12,28 +14,35 @@ namespace Lunarcrush_Project
 
     public class Program
     {
-        private const string URL = "https://api.lunarcrush.com/v2?data=assets?";
-        private static string urlParameters = "?api_key=y2p6tcgi3eoih3b9fbol0f";
+        private const string URL = "https://api.lunarcrush.com/v2?data=assets";
+        private string urlParameters = "&key=maba1tuhx6eib93jtv485";
+        private string symbol = "&symbol=LTC";
 
         static void Main(string[] args)
         {
+            var program = new Program();
+
+            var parameters = program.urlParameters + program.symbol;
+
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
+
+            Console.WriteLine(client.BaseAddress);
+
             client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result; 
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(URL + parameters).Result;
             if (response.IsSuccessStatusCode)
             {
-                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;
-                foreach (var d in dataObjects)
-                {
-                    Console.WriteLine("{0}", d.Name);
-                }
+                var dataObjects = response.Content.ReadAsStringAsync().Result;
+                JObject data = (JObject)JsonConvert.DeserializeObject(dataObjects);
+                Console.Write(data["config"]);
             }
             else
             {
                 Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
             }
+
             client.Dispose();
         }
     }
