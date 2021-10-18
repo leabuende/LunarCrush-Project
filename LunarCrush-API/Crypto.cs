ce2b3552f9896f3d7ea6034726ef88923ac3e353
Faris Chtatou <faris.manage@gmail.com>
2021-10-19 00:32:25 +0200

    Add léa work refacto

---
+++ LunarCrush-API/Crypto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Frequency
+{
+    public class Crypto
+    {
+        public Crypto(int frequency, string name)
+        {
+            this.frequency = frequency;
+            this.name = name;
+        }
+        public int frequency { get; set; }
+        public string name { get; set; }
+    }
+}
